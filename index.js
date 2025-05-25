bba4e54729ee4536353918ebeb9dc1bc3e75abeb
Jonty Leslie <83710197+JLesDev@users.noreply.github.com>
2025-05-25 21:42:04 +1000

    Bug fixes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ globalThis.won = 0;
 globalThis.cities = new Map;
 globalThis.city = "";
 globalThis.guesses = 0;
+globalThis.rands = Math.floor(Math.random() * cityL.length);
 
 async function getCities(){
     const baseURL = window.location.origin;
@@ -24,8 +25,6 @@ cities.set('Dublin', ['https://i.ibb.co/JRKHQfVv/dublin1.png','https://i.ibb.co/
 async function onLoad(){
     try {
         globalThis.cityL = await getCities();
-
-        const rands = Math.floor(Math.random() * cityL.length);
 
         city = cityL[rands];
         // For debugging: console.log("Loaded city:", city);
